297981f00291839aaffca13d7a91047aa8dac6ca
Reghan <reghansan@gmail.com>
2026-05-02 01:28:44 +0300

    Добавлен роутер

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,46 +1,9 @@
-import { Preloader } from '@krgaa/react-developer-burger-ui-components';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useDispatch } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 
-import { AppHeader } from '@components/app-header/app-header';
-import { BurgerConstructor } from '@components/burger-constructor';
-import { BurgerIngredients } from '@components/burger-ingredients';
-import { useGetIngredientsQuery } from '@services/ingredientsApi';
-import { selectConsturctorIngredient } from '@services/slices/constructor-slice';
-
-import type { TIngredient } from '@utils/types';
-
-import styles from './app.module.css';
+import { router } from './router';
 
 export const App = (): React.JSX.Element => {
-  const { isLoading } = useGetIngredientsQuery();
-  const dispatch = useDispatch();
-
-  const handleDrop = (item: TIngredient): void => {
-    dispatch(selectConsturctorIngredient(item));
-  };
-
-  return (
-    <div className={styles.app}>
-      <AppHeader />
-      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-        Соберите бургер
-      </h1>
-      <main className={`${styles.main} pl-5 pr-5`}>
-        <DndProvider backend={HTML5Backend}>
-          {isLoading ? (
-            <Preloader />
-          ) : (
-            <>
-              <BurgerIngredients />
-              <BurgerConstructor onDropHandler={handleDrop} />
-            </>
-          )}
-        </DndProvider>
-      </main>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,43 @@
+import { Preloader } from '@krgaa/react-developer-burger-ui-components';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useDispatch } from 'react-redux';
+
+import { AppHeader } from '@components/app-header/app-header';
+import { BurgerConstructor } from '@components/burger-constructor';
+import { BurgerIngredients } from '@components/burger-ingredients';
+import { useGetIngredientsQuery } from '@services/ingredientsApi';
+import { selectConsturctorIngredient } from '@services/slices/constructor-slice';
+
+import type { TIngredient } from '@utils/types';
+
+import styles from './home.module.css';
+export const HomePage = (): React.JSX.Element => {
+  const { isLoading } = useGetIngredientsQuery();
+  const dispatch = useDispatch();
+
+  const handleDrop = (item: TIngredient): void => {
+    dispatch(selectConsturctorIngredient(item));
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
+        Соберите бургер
+      </h1>
+      <main className={`${styles.main} pl-5 pr-5`}>
+        <DndProvider backend={HTML5Backend}>
+          {isLoading ? (
+            <Preloader />
+          ) : (
+            <>
+              <BurgerIngredients />
+              <BurgerConstructor onDropHandler={handleDrop} />
+            </>
+          )}
+        </DndProvider>
+      </main>
+    </div>
+  );
+};
